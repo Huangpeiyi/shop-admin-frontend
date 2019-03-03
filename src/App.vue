59aa8f36868af0837686394e636ce914c5881fcd
hpy617559 <1103876865@qq.com>
2019-03-03 09:13:16 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,188 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 调用Element UI组件 -->
+    <div class="container">
+      <h3>登录</h3>
+      <!-- <div class="inputBox">
+        <label>账号：</label>
+        <el-input v-model="username" placeholder="请输入账号"></el-input>
+      </div>
+      <div class="inputBox">
+        <label>密码：</label>
+        <el-input v-model="password" placeholder="请输入密码" show-password></el-input>
+      </div>
+      <div class="butBox">
+        <el-button type="primary" @click="handleSubmit">提交</el-button>
+        <el-button @click="handleReset">重置</el-button>
+      </div>-->
+      <el-form
+        :model="ruleForm2"
+        status-icon
+        :rules="rules2"
+        ref="ruleForm2"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="账号" prop="age">
+          <el-input v-model.number="ruleForm2.age"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- <el-form-item label="确认密码" prop="checkPass">
+          <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
+        </el-form-item> -->
+        
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+          <el-button @click="resetForm('ruleForm2')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+/* export default {
+  name: "app",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.username);
+      console.log(this.password);
+    },
+    handleReset() {
+      this.username = "";
+      this.password = "";
+    }
+  },
+  mounted() {
+    //请求数据
+    // this.$axios.get('https://api.github.com/users').then(res => {
+    //   console.log(res.data);
+    // })
   }
-}
+}; */
+
+ export default {
+    data() {
+      var checkAge = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('账号不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm2.checkPass !== '') {
+            this.$refs.ruleForm2.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm2.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        ruleForm2: {
+          pass: '',
+          checkPass: '',
+          age: ''
+        },
+        rules2: {
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          age: [
+            { validator: checkAge, trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
 </script>
 
-<style>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .container {
+    width: 400px;
+    height: auto;
+    padding-bottom: 100px;
+    h3 {
+      text-align: center;
+      color: rgb(25, 163, 255);
+      line-height: 60px;
+    }
+    .inputBox {
+      display: flex;
+      margin-bottom: 20px;
+      label {
+        width: 60px;
+        line-height: 32px;
+      }
+     /*  el-input {
+        flex: 1;
+      } */
+    }
+    .butBox {
+      text-align: center;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//1.引入element-ui
+import ElementUI from "element-ui";
+
+//2.引入样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 1.引入axios
+import axios from 'axios';
+
+// import store from './store'
+// import router from './router'
+//3.全局注册组件
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+
+// 2.把axios绑定到vue实例的属性$axios
+Vue.prototype.$axios = axios;
 
 new Vue({
-  render: h => h(App),
+  // store,
+  // router,
+  render: h => h(App)
 }).$mount('#app')
